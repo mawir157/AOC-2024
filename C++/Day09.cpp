008cda4f8320172894ed650eaf6e1e6251436059
mawir157 <jamesmthompson2@gmail.com>
2024-12-09 20:12:03 +0000

    [09] :suspect::suspect:

--- C++/Day09.cpp
+++ C++/Day09.cpp
@@ -1,4 +1,4 @@
-#ifdef DAY09
+// #ifdef DAY09
 
 #include "AH.h"
 
@@ -8,6 +8,10 @@ namespace Day09
 		int start;
 		int length;
 		int id;
+
+		File(int start, int length, int id) :
+		start{start}, length{length}, id{id}
+		{}
 	};
 
 	std::vector<int> parseInput(std::string s)
@@ -35,22 +39,24 @@ namespace Day09
 	{
 		std::vector<File> files;
 		std::vector<File> gaps;
-		
+
 		bool writing = true;
 		int id = 0;
 		int pos = 0;
 		for (auto c : s) {
 			int sz = c - '0';
-			if (writing) {
-				files.emplace_back(pos, sz, id);
-				id++;
-			} else {
-				gaps.emplace_back(pos, sz, -1);
+			if (sz != 0) {
+				if (writing) {
+					files.emplace_back(pos, sz, id);
+					id++;
+				} else {
+					gaps.emplace_back(pos, sz, -1);
+				}
 			}
 			pos += sz;
 			writing = !writing;
 		}
-
+		
 		return std::make_pair(files, gaps);
 	}
 
@@ -58,7 +64,7 @@ namespace Day09
 	{
 		auto it_l = std::find_if (fs.begin(), fs.end(), [](int x) { return x < 0;} );
 		auto it_r = std::find_if (fs.rbegin(), fs.rend(), [](int x) { return x >= 0;});
-
+		
 		int counter = 0;
 		while (it_l < it_r.base() - 1) {
 			counter++;
@@ -73,14 +79,101 @@ namespace Day09
 
 		return;
 	}
-
+	
+	// just append to files/gaps then reorder by start
 	void defrag2(std::vector<File> & files, std::vector<File> & gaps)
 	{
+		int to_move = 0;
+		for (auto f : files) {
+			if (f.id > to_move) {
+				to_move = f.id;
+			}
+		}
+
+		while (to_move >= 0) {
+			for (auto & f : files) {
+				bool moved = false;
+				if (f.id != to_move) { //cba to do this efficiently yet
+					continue;
+				}
+				// this hole was made for me
+				for (int gi = 0; gi < (int)gaps.size(); gi++) {
+					auto gap = gaps[gi];
+					auto gapW = gap.length;
+
+					if (gap.start > f.start) {
+						continue;
+					}
+
+					if (f.length == gapW) {
+						gaps.emplace_back(f.start, f.length, -1);
+						f.start = gap.start;
+						gaps.erase (gaps.begin()+gi);
+						moved = true;
+						break;
+					} else if (f.length <= gapW) {
+						gaps.emplace_back(f.start, f.length, -1);
+						f.start = gap.start;
+						gaps[gi].length -= f.length;
+						gaps[gi].start += f.length;
+						moved = true;
+						break;
+					}
+				}
+				
+				if (moved) {
+					std::sort(files.begin(), files.end(), [](const File &a, const File &b)
+						{
+							return a.start < b.start;
+						});
+					std::sort(gaps.begin(), gaps.end(), [](const File &a, const File &b)
+						{
+							return a.start < b.start;
+						});
+
+					// combine gaps
+					std::vector<File> new_gaps;
+					int cur_start = -1;
+					int cur_length =0;
+					for (auto g : gaps) {
+						if (cur_start == -1) {
+							cur_start = g.start;
+						}
+
+						if ((cur_start + cur_length) == g.start) {
+							cur_length += g.length;
+						} else {
+							new_gaps.emplace_back(cur_start, cur_length, -1);
+							cur_start = g.start;
+							cur_length = g.length;
+						}
+					}
+					new_gaps.emplace_back(cur_start, cur_length, -1);
+					gaps = new_gaps;
+					break;
+				} else {
+					break;
+				}
+			}
+			to_move--;
+		}
+
 		return;
 	}
+	
+	int64_t checkSum2(std::vector<File> & fs)	{
+		int64_t sum = 0;
 
-	int64_t checkSum(std::vector<int> & fs)
-	{
+		for (auto f : fs) {
+			for (auto s = f.start; s < f.start+f.length; s++) {
+				sum += s * f.id;
+			}
+		}
+
+		return sum;
+	}
+
+	int64_t checkSum(std::vector<int> & fs)	{
 		int64_t checkSum = 0;
 		for (int64_t i = 0; i < (int64_t)fs.size(); i++) {
 			auto v = fs[i];
@@ -93,18 +186,18 @@ namespace Day09
 		return checkSum;
 	}
 
-	int Run(const std::string& filename)
-	{
-		const auto grid = AH::ReadTextFile(filename);
-		auto fs = parseInput(grid[0]);
-
+	int Run(const std::string& filename)	{
+		const auto is = AH::ReadTextFile(filename);
+		auto fs = parseInput(is[0]);
 		defrag(fs);
+		auto p1 = checkSum(fs);
 
-		AH::PrintSoln(9, checkSum(fs), 1);
+		auto [files, gaps] = parseInput2(is[0]);
+		defrag2(files, gaps);
+		auto p2 = checkSum2(files);
 
+		AH::PrintSoln(9, p1, p2);
 		return 0;
 	}
-
 }
-
-#endif
+// #endif
