43f9b4a67e12fa2e9393afb1f2c1575997fdea73
mawir157 <jamesmthompson2@gmail.com>
2024-12-13 13:16:15 +0000

    [12] 🐸🐸

--- C++/Day12.cpp
+++ C++/Day12.cpp
@@ -5,7 +5,7 @@
 namespace Day12
 {
 
-	typedef std::pair<int, int> Region;
+	typedef std::tuple<int, int, int> Region;
 	struct Pos {
 		int r;
 		int c;
@@ -34,27 +34,28 @@ namespace Day12
 
 	std::vector<Region> part1(std::map<Pos, char> m)
 	{
-		std::vector<std::pair<int, int>> rs;
+		std::vector<Region> rs;
 		auto m_copy = m;
 		std::list<Pos> q;
 		while (m_copy.size() > 0)
 		{ 
-			auto p = m_copy.begin()->first; // random starting point
+			auto p = m_copy.begin()->first; // 'random' starting point
 			auto ch = m_copy.begin()->second;
 			m_copy.erase(p);
 			q.push_back(p);
 
 			int r_size = 0;
 			int r_peri = 0;
-			int r_walls = 0;
-
-			std::cout << ch << " - ";
+			int r_corners = 0;
 
 			while(q.size() > 0) {
 				r_size++;
 				auto n = q.front();
 				q.pop_front();
-				int corners = 0;
+				bool n_f = false;
+				bool e_f = false;
+				bool s_f = false;
+				bool w_f = false;
 
 				// find nbrs
 				auto North = Pos{.r=n.r-1, .c=n.c};
@@ -63,7 +64,7 @@ namespace Day12
 					q.push_back(North);
 				} else if (m[North] != ch) {
 					r_peri++;
-					corners++;
+					n_f = true;
 				}
 
 				auto East = Pos{.r=n.r, .c=n.c+1};
@@ -72,7 +73,7 @@ namespace Day12
 					q.push_back(East);
 				} else if (m[East] != ch) {
 					r_peri++;
-					corners++;
+					e_f = true;
 				}
 
 				auto South = Pos{.r=n.r+1, .c=n.c};
@@ -81,7 +82,7 @@ namespace Day12
 					q.push_back(South);
 				} else if (m[South] != ch) {
 					r_peri++;
-					corners++;
+					s_f = true;
 				}
 
 				auto West = Pos{.r=n.r, .c=n.c-1};
@@ -90,14 +91,52 @@ namespace Day12
 					q.push_back(West);
 				} else if (m[West] != ch) {
 					r_peri++;
-					corners++;
+					w_f = true;
 				}
 
-				std::cout << "+" << (corners - 1) << " ";
-				r_walls += (corners - 1);
+				
+				// corner counting!
+				// +ve corners
+				if (n_f && e_f) {
+					r_corners++;
+				}
+				if (e_f && s_f) {
+					r_corners++;
+				}
+				if (s_f && w_f) {
+					r_corners++;
+				}
+				if (w_f && n_f) {
+					r_corners++;
+				}
+
+				// -ve corners
+				if (!(n_f || e_f)) { // not blocked to north or east
+					auto NE = Pos{.r=n.r-1, .c=n.c+1};
+					if (m[NE] != ch) { // this is a different region...
+						r_corners++; // .. so this is a corner
+					}
+				}
+				if (!(e_f || s_f)) { // not blocked to east or south
+					auto SE = Pos{.r=n.r+1, .c=n.c+1};
+					if (m[SE] != ch) { // this is a different region...
+						r_corners++; // .. so this is a corner
+					}
+				}
+				if (!(s_f || w_f)) { // not blocked to south or west
+					auto SW = Pos{.r=n.r+1, .c=n.c-1};
+					if (m[SW] != ch) { // this is a different region...
+						r_corners++; // .. so this is a corner
+					}
+				}
+				if (!(w_f || n_f)) { // not blocked to west or north
+					auto NW = Pos{.r=n.r-1, .c=n.c-1};
+					if (m[NW] != ch) { // this is a different region...
+						r_corners++; // .. so this is a corner
+					}
+				}
 			}
-			rs.emplace_back(r_size, r_peri);
-			std::cout << r_walls << "\n";
+			rs.emplace_back(r_size, r_peri, r_corners);
 		}
 
 		return rs;
@@ -108,10 +147,11 @@ namespace Day12
 		auto m = parseInput(is);
 		auto pairs = part1(m);
 		int p1 = 0;
-		for (auto [a,p] : pairs) {
-			p1 += a*p;
-		}
 		int p2 = 0;
+		for (auto [a,p,w] : pairs) {
+			p1 += a*p;
+			p2 += a*w;
+		}
 
 		AH::PrintSoln(12, p1, p2);
 		return 0;
